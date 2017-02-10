c7fa78b27b99adca4c80abbb65fb4e1726e38cc5
fifsky <fifsky@aliyun.com>
2017-02-10 17:37:22 +0800

    add pagination layouts

--- helpers/util.php
+++ helpers/util.php
@@ -1,11 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 蔡旭东 fifsky@gmail.com
- * Date: 14/7/30
- * Time: 下午12:01
- */
-
 function fee_format($fee) {
     return number_format($fee / 100, 2);
+}
+
+//检验验证码
+function check_vcode($captcha_code) {
+    $session = session();
+    $vcode   = $session->get('vcode');
+    $vcode   = $vcode ? explode('|', $vcode) : '';
+    $session->delete('vcode');
+
+    if (!$vcode || !isset($vcode[1]) || strtolower($vcode[0]) != strtolower($captcha_code) || $vcode[1] + 60 < time()) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * 生成资源树
+ *
+ * @param        $items
+ * @param string $id
+ * @param string $pid
+ * @param string $son
+ *
+ * @return array
+ */
+function get_tree($items, $id = 'cate_id', $pid = 'parent_cate_id', $son = 'cate_child')
+{
+    $tmpMap = array(); //临时扁平数据
+
+    foreach ($items as $item) {
+        $tmpMap[$item[$id]] = $item;
+    }
+
+    foreach ($tmpMap as $item) {
+        $tmpMap[$item[$pid]][$son][$item[$id]] = &$tmpMap[$item[$id]];
+    }
+    return isset($tmpMap[0][$son]) ? $tmpMap[0][$son] : array();
 }
---
+++ views/layouts/page.php
@@ -0,0 +1,36 @@
+<?php
+parse_str($_SERVER['QUERY_STRING'], $params);
+unset($params['page']);
+$page_url  = isset($page_url) ? $page_url : url(parse_url(request()->uri(), PHP_URL_PATH) . ($params ? '?' . http_build_query($params) : ''));
+$page_url .= (strpos($page_url, '?') === false ? '?' : '&') . 'page=';
+?>
+<ul class="pagination pagination-sm nomargin">
+    <li><a href="javascript:void(0)">总数:<?php echo $pager->getTotalCount() ?></a></li>
+    <li<?php echo $pager->getCurrPage() <= 1 ? ' class=disabled' : ''; ?>>
+        <a title="第一页" href="<?php echo $pager->getCurrPage() <= 1 ? 'javascript:void(0)' : $page_url . '1' ?>">&lt;&lt;</a></li>
+    <?php if ($pager->hasPrePage()) { ?>
+        <li><a href="<?php echo $page_url . ($pager->getCurrPage() - 1); ?>" title="上一页">&lt;</a></li>
+    <?php } else { ?>
+        <li class="disabled"><a href="javascript:void(0)">&lt;</a></li>
+    <?php } ?>
+    <?php $near_pages = $pager->getNearerPages();
+    foreach ($near_pages as $near_page) { ?>
+        <?php if ($pager->getCurrPage() == $near_page) { ?>
+            <li class="active"><a href="javascript:void(0)"><?php echo $near_page ?></a></li>
+        <?php } else { ?>
+            <li>
+                <a href="<?php echo $page_url . $near_page; ?>"><?php echo $near_page; ?></a>
+            </li>
+        <?php } ?>
+    <?php } ?>
+    <?php if ($pager->hasNextPage()) { ?>
+        <li>
+            <a title="下一页" href="<?php echo $page_url . ($pager->getCurrPage() + 1); ?>">&gt;</a>
+        </li>
+    <?php } else { ?>
+        <li class="disabled"><a href="javascript:void(0)">&gt;</a></li>
+    <?php } ?>
+    <li<?php echo $pager->getCurrPage() >= $pager->getTotalPage() ? ' class="disabled"' : '' ?>>
+        <a title="最后一页" href="<?php echo $pager->getCurrPage() >= $pager->getTotalPage() ? 'javascript:void(0)' : $page_url . $pager->getTotalPage() ?>">&gt;&gt;</a>
+    </li>
+</ul>
