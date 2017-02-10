a7efa5043b1b1e1039399d8120cd75f0c97faa05
fifsky <fifsky@aliyun.com>
2017-02-10 22:45:45 +0800

    update page layout

--- views/layouts/page.php
+++ views/layouts/page.php
@@ -1,13 +1,12 @@
 <?php
-$page_url = url()->make(request()->uri())->remove(['page'])->url();
-?>
-<?php if (is_object($pager)) { ?>
+if (isset($pager) && is_object($pager)) {
+    ?>
     <ul class="pagination pagination-sm nomargin">
         <li><a href="javascript:void(0)">总数:<?php echo $pager->getTotalCount() ?></a></li>
         <li<?php echo $pager->getCurrPage() <= 1 ? ' class=disabled' : ''; ?>>
-            <a title="第一页" href="<?php echo $pager->getCurrPage() <= 1 ? 'javascript:void(0)' : $page_url . '1' ?>">&lt;&lt;</a></li>
+            <a title="第一页" href="<?php echo $pager->getCurrPage() <= 1 ? 'javascript:void(0)' : url(request()->uri(), ['page' => 1]); ?>">&lt;&lt;</a></li>
         <?php if ($pager->hasPrePage()) { ?>
-            <li><a href="<?php echo $page_url . ($pager->getCurrPage() - 1); ?>" title="上一页">&lt;</a></li>
+            <li><a href="<?php echo url(request()->uri(), ['page' => ($pager->getCurrPage() - 1)]); ?>" title="上一页">&lt;</a></li>
         <?php } else { ?>
             <li class="disabled"><a href="javascript:void(0)">&lt;</a></li>
         <?php } ?>
@@ -17,20 +16,19 @@ $page_url = url()->make(request()->uri())->remove(['page'])->url();
                 <li class="active"><a href="javascript:void(0)"><?php echo $near_page ?></a></li>
             <?php } else { ?>
                 <li>
-                    <a href="<?php echo $page_url . $near_page; ?>"><?php echo $near_page; ?></a>
+                    <a href="<?php echo url(request()->uri(), ['page' => $near_page]); ?>"><?php echo $near_page; ?></a>
                 </li>
             <?php } ?>
         <?php } ?>
         <?php if ($pager->hasNextPage()) { ?>
             <li>
-                <a title="下一页" href="<?php echo $page_url . ($pager->getCurrPage() + 1); ?>">&gt;</a>
+                <a title="下一页" href="<?php echo url(request()->uri(), ['page' => ($pager->getCurrPage() + 1)]); ?>">&gt;</a>
             </li>
         <?php } else { ?>
             <li class="disabled"><a href="javascript:void(0)">&gt;</a></li>
         <?php } ?>
         <li<?php echo $pager->getCurrPage() >= $pager->getTotalPage() ? ' class="disabled"' : '' ?>>
-            <a title="最后一页" href="<?php echo $pager->getCurrPage() >= $pager->getTotalPage() ? 'javascript:void(0)' : $page_url . $pager->getTotalPage() ?>">
-                &gt;&gt;</a>
-        </li>
+            <a title="最后一页" href="<?php echo $pager->getCurrPage() >= $pager->getTotalPage() ? 'javascript:void(0)' : url(request()->uri(),
+                ['page' => $pager->getTotalPage()]); ?>">&gt;&gt;</a></li>
     </ul>
 <?php } ?>
