6f6c04b1e21792bd1aa91a6c6d093feffdd2a996
fifsky <fifsky@aliyun.com>
2017-02-20 17:04:18 +0800

    validation support array size

--- config/fstat.php
+++ config/fstat.php
@@ -7,24 +7,16 @@
 
 if (ENVIRON == 'local') {
     return [
-        'use_db'       => 'stat',
-        'is_stat'      => true,
-        'db_prefix'    => 'test_',
-        'redis_config' => [
-            'host'    => '127.0.0.1',
-            'port'    => '6379',
-            'timeout' => 0.1,
-        ]
+        'use_db'    => 'default',
+        'is_stat'   => true,
+        'db_prefix' => 'test_',
+        'use_redis' => 'default',
     ];
 } else {
     return [
-        'use_db'       => 'stat',
-        'is_stat'      => true,
-        'db_prefix'    => 'test_',
-        'redis_config' => [
-            'host'    => '127.0.0.1',
-            'port'    => '6379',
-            'timeout' => 0.1,
-        ]
+        'use_db'    => 'default',
+        'is_stat'   => true,
+        'db_prefix' => 'test_',
+        'use_redis' => 'default',
     ];
 }
