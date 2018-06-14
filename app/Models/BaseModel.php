122df7e401ab50f908ef7ee4335c7d685e3cb721
fifsky <fifsky@gmail.com>
2018-06-14 17:23:29 +0800

    remove ssdb trait

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -8,20 +8,9 @@ namespace App\Models;
  */
 
 use Very\Model;
-use App\Traits\SsdbTrait;
 
 class BaseModel extends Model
 {
-    use SsdbTrait;
-
-    //缓存时间
-    const CACHE_WEEK_TTL   = 604800;//7天
-    const CACHE_DAY_TTL    = 86400;//1天
-    const CACHE_HOUR_TTL   = 3600;//1小时
-    const CACHE_MIN_TTL    = 60;//1分钟
-    const ACCESS_TOKEN_TTL = 180;//180秒
-
-
     public function field($table)
     {
         return config('field.' . $this->use_db . '.' . $table, []);
